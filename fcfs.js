ae466b6de63484dcfc1c1d609175863edf7083a0
Mateus de Paula Rezende <110778640+mateusp-rezende@users.noreply.github.com>
2026-06-09 21:42:16 -0300

    Update fcfs.js

--- fcfs.js
+++ fcfs.js
@@ -1,44 +1,9 @@
 function calcularFCFS(posicaoInicial, sequenciaDeTrilhas) {
     /*
         FCFS = First Come, First Served
-        Em português: Primeiro a chegar, primeiro a ser atendido.
-
-        No escalonamento de disco, isso significa:
-        o braço do disco vai atender as trilhas exatamente
-        na ordem em que o usuário digitou.
+        O caminho percorrido é exatamente a ordem em que as requisições chegaram.
+        Os cálculos matemáticos de distância já são tratados globalmente pelo script.js.
     */
-
-    let caminhoPercorrido = [];
-    let movimentos = [];
-    let movimentoTotal = 0;
-
-    let posicaoAtual = posicaoInicial;
-
-    caminhoPercorrido.push(posicaoAtual);
-
-    for (let i = 0; i < sequenciaDeTrilhas.length; i++) {
-        let proximaTrilha = sequenciaDeTrilhas[i];
-
-        let movimento = Math.abs(proximaTrilha - posicaoAtual);
-
-        movimentoTotal = movimentoTotal + movimento;
-
-        movimentos.push({
-            origem: posicaoAtual,
-            destino: proximaTrilha,
-            distancia: movimento
-        });
-
-        caminhoPercorrido.push(proximaTrilha);
-
-        posicaoAtual = proximaTrilha;
-    }
-
-
-    return {
-        algoritmo: "FCFS",
-        caminhoPercorrido: caminhoPercorrido,
-        movimentos: movimentos,
-        movimentoTotal: movimentoTotal
-    };
+    
+    return [...sequenciaDeTrilhas];
 }
